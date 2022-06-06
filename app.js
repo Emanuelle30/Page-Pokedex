70a8c19023542551d7eba7408cec11d9fa583440
Emanuelle Cavalcante <emanuelle.cavalcante4@gmail.com>
2022-06-05 21:41:16 -0300

    Comentários e organização

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+
+//Comunica com Api (requisições)
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
+//Array de 45 itens 
 const generatePokemomPromises = () => Array(45).fill().map((_, index) =>
     fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
+/* adiciona o novo elemento criado e seu conteúdo ao DOM 
+redução do array*/
 const generateHTML = pokemons => pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
+//juntar os valores
         accumulator += `
        <li class= "card ${types[0]}">
        <img class= "card-image" alt= "${pokemon.name} "src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
@@ -20,7 +26,7 @@ const insertPokemonsIntoPage = pokemons => {
     const ul = document.querySelector('[data-js="pokedex"]')
     ul.innerHTML = pokemons
 }
-
+//retorno de objeto assíncrono
 const pokemonPromises = generatePokemomPromises()
 
 Promise.all(pokemonPromises)
